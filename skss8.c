ccf9577660226f26ab1b3a8fd57b97f478b7de7a
kiruthiMuthu <kiruthiIDOK@gmail.com>
2017-06-07 15:23:12 +0530

    Add files via upload

---
+++ skss8.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+
+{
+ 
+int a,b,c;
+
+ printf("enter a number");
+ 
+scanf("%d,%d,%d",&a,&b,&c);
+
+if(a>b&&a>c)
+
+{
+    
+printf("a is gerater");
+   
+ }
+    
+else if(b>a&&b>c)
+    
+{
+        
+printf("b is greater");
+  
+      }
+       
+ else if(c>a&&c>b)
+
+{
+    
+printf("c is greater ");
+
+}
+
+return 0;
+
+}
