e2841453bdaf403f2b8381c1a88fee860af062b3
kiruthiMuthu <kiruthiIDOK@gmail.com>
2017-06-07 15:11:38 +0530

    Add files via upload

---
+++ skss7.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int main() 
+
+{
+    
+char c;
+   
+ printf("enter a character");
+    
+scanf("%c",&c);
+   
+ if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+
+{
+    
+printf("the character is alphabet");
+
+}
+
+else
+
+{
+
+printf("the character is not alphabet");
+
+}
+
+return 0;
+
+}
